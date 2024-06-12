b13a9c3ad3506114b62837357081d616ef160383
Anmol Manchanda <manchandaanmol@icloud.com>
2024-06-12 13:21:43 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+
+function App(){
+    return (
+        <div>
+            <h1>Hello, React!</h1>
+            <p>Welcome to my first React website.</p>
+        </div>
+    );
+}
+
+export default App;
